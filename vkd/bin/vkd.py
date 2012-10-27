afba6f7b68f08dd3d14b7666120ca705ea434541
Maxim Syabro <syabro@gmail.com>
2012-10-28 01:31:30 +0400

    Fix

--- vkd/bin/vkd.py
+++ vkd/bin/vkd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-from download_post import download_post
+from vkd.download_post import download_post
 if len(sys.argv)==1:
     print 'usage: vkd url'
     exit()
